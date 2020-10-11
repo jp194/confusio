7ebdf81a54d959ef89151a6fe6953af2d1dbdb6a
Jenil pipada <jenil.p194@gmail.com>
2020-10-11 12:07:11 +0530

    Combine reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,20 @@
-import {createStore} from 'redux';
-import {initialState,reducer} from './reducer';
+import {createStore,combineReducers} from 'redux';
+import {Dishes} from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
+
 
 export const configureStore=()=>{
 	
 	const store=createStore(
-	reducer,
-	initialState
+	    combineReducers({
+			dishes:Dishes,
+			comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+		})
+	  
 	);
 	return store;
 }
